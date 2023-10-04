9c3f716c93a3b694621b8af696bf14ea5333340b
Emma Stanley <69406048+estanley16@users.noreply.github.com>
2023-10-04 15:54:57 -0600

    Add files via upload

---
+++ model/example_model_pipeline.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+#run full model pipeline
+exps=(experiment_name1 experiment_name2 experiment_name3)
+arrs=(TP TN)
+
+for exp in "${exps[@]}"
+do
+
+      echo training model for $exp
+      python model_3d.py --expname $exp #train model
+
+      echo evaluating $exp
+      python evaluate_3d.py --exp_name $exp #evaluate model
+
+      for arr in "${arrs[@]}"
+      do
+
+              echo generating saliency for $exp $arr
+              python xai_3d.py --exp_name $exp --array $arr --bias_label 0 #saliency maps for non-bias group
+              python xai_3d.py --exp_name $exp --array $arr --bias_label 1 #saliency maps for bias group
+              python avg_saliency_maps.py --exp_name $exp --array $arr --bias 1 #average saliency maps
+      done
+
+done
